c78dc4915efac8d0c64a6009b9f2aee772fc56d7
Felegyi Mihály Patrik <60849611+FMisi@users.noreply.github.com>
2022-05-07 14:02:08 +0200

    Linking_Layout

--- app/src/main/java/com/example/myapplikaciosokadik/LoginActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/LoginActivity.java
@@ -2,20 +2,39 @@ package com.example.myapplikaciosokadik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.*;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.*;
+import android.widget.*;
+import androidx.appcompat.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     private EditText loginEmail, loginPwd;
     private Button loginBtn;
-    //private
+    private TextView loginQn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        toolbar = findViewById(R.id.loginToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Bejelentkezés");
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPwd = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginQn = findViewById(R.id.loginPageQuestion);
+
+        loginQn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
@@ -2,15 +2,40 @@ package com.example.myapplikaciosokadik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.*;
+import android.widget.*;
+import androidx.appcompat.widget.Toolbar;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText RegEmail, RegPwd;
+    private Button RegBtn;
+    private TextView RegQn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_registration);
+
+        toolbar = findViewById(R.id.RegistrationToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Regisztráció");
+
+        RegEmail = findViewById(R.id.RegistrationEmail);
+        RegPwd = findViewById(R.id.RegistrationPassword);
+        RegBtn = findViewById(R.id.RegistrationButton);
+        RegQn = findViewById(R.id.RegistrationPageQuestion);
+
+        RegQn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
