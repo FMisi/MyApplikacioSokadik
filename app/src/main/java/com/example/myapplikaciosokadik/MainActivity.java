185e0bf7257a7e7d728d8fe5b29d559898146d22
Felegyi Mihály Patrik <60849611+FMisi@users.noreply.github.com>
2022-05-07 09:00:54 +0200

    SPLASH

--- app/src/main/java/com/example/myapplikaciosokadik/MainActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.myapplikaciosokadik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.os.Handler;
+import android.view.*;
+import android.view.animation.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.text);
+
+        imageView.setAnimation(topAnim);
+        textView.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH);
     }
 }
