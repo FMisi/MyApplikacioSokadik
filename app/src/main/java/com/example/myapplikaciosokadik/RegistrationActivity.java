6d473a56213f750d62078dbc81aac7bd2c9fd299
Felegyi Mihály Patrik <60849611+FMisi@users.noreply.github.com>
2022-05-07 11:13:54 +0200

    Regi+login

--- app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
@@ -4,18 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
-    private EditText loginEmail, loginPwd;
-    private Button loginBtn;
-    //private
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 }
