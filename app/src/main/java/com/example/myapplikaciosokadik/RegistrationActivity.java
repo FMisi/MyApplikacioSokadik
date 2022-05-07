e18e2449e72eee7b57d678f0432c821169d5626d
Felegyi Mihály Patrik <60849611+FMisi@users.noreply.github.com>
2022-05-07 17:44:30 +0200

    Connect_android_with_firebase_database

--- app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/RegistrationActivity.java
@@ -1,18 +1,31 @@
 package com.example.myapplikaciosokadik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.*;
 import android.widget.*;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.*;
+import com.google.firebase.auth.*;
+
+import java.util.Objects;
+
 public class RegistrationActivity extends AppCompatActivity {
+
     private Toolbar toolbar;
+
     private EditText RegEmail, RegPwd;
     private Button RegBtn;
     private TextView RegQn;
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +35,56 @@ public class RegistrationActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.RegistrationToolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Regisztráció");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Regisztráció");
+
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
 
         RegEmail = findViewById(R.id.RegistrationEmail);
         RegPwd = findViewById(R.id.RegistrationPassword);
         RegBtn = findViewById(R.id.RegistrationButton);
         RegQn = findViewById(R.id.RegistrationPageQuestion);
 
-        RegQn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
+        RegQn.setOnClickListener((v) -> {
+            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
 
+        RegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = RegEmail.getText().toString().trim();
+                String password = RegPwd.getText().toString().trim();
 
+                if (TextUtils.isEmpty(email)){
+                    RegEmail.setError("Email megadása szükséges!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    RegPwd.setError("Jelszó megadása szükséges!");
+                    return;
+                } else{
+                    loader.setMessage("Regisztráció folyamatban");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else{
+                                String error = task.getException().toString();
+                                Toast.makeText(RegistrationActivity.this, "Sikertelen regisztráció" + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
