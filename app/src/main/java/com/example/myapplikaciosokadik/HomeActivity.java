d0d873237995de4f674eaf1bf50b0e9e28b9b691
Felegyi Mihály Patrik <60849611+FMisi@users.noreply.github.com>
2022-05-08 08:08:56 +0200

    Creating_AlertDialog

--- app/src/main/java/com/example/myapplikaciosokadik/HomeActivity.java
+++ app/src/main/java/com/example/myapplikaciosokadik/HomeActivity.java
@@ -1,17 +1,57 @@
 package com.example.myapplikaciosokadik;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import androidx.appcompat.widget.*;
+import androidx.recyclerview.widget.*;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.*;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.homeToolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Jegyzet Készítő App");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+    }
+
+    private void addTask() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.input_file, null);
+        myDialog.setView(myView);
+
+        AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
